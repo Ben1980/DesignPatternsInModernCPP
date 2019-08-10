d02761a8ed5562d7f8d9cd9e921ec7e5b74386cd
Benjamin Mahr <Ben1980@users.noreply.github.com>
2019-08-10 13:40:15 +0200

    Create decorator.h

---
+++ decorator.h
@@ -0,0 +1,67 @@
+#include <string>
+#include <sstream>
+
+// A Rose can be decorated with colors
+// Example:
+// Rose rose;
+// RedFlower red_rose(rose);
+// RedFlower red_red_rose(red_rose);
+// BlueFlower blue_red_rose(red_rose);
+// BlueFlower blue_rose(rose);
+// RedFlower red_blue_red_rose(blue_red_rose);
+
+struct Flower
+{
+  virtual std::string str() = 0;
+};
+
+struct Rose : Flower
+{
+  std::string str() override {
+    std::ostringstream oss;
+    oss << "A rose";
+    return oss.str();
+  }
+};
+
+struct RedFlower : Flower
+{
+  Flower &mFlower;
+  const std::string color;
+
+  RedFlower(Flower &flower) : mFlower(flower), color("red") { }
+
+  std::string str() override {
+    std::string ret = mFlower.str();
+
+    if (ret.find("that is") == std::string::npos) {
+      ret += " that is " + color;
+    }
+    else if (ret.find(color) == std::string::npos) {
+      ret += " and " + color;
+    }
+
+    return ret;
+  }
+};
+
+struct BlueFlower : Flower
+{
+  Flower &mFlower;
+  const std::string color;
+
+  BlueFlower(Flower &flower) : mFlower(flower), color("blue") { }
+
+  std::string str() override {
+    std::string ret = mFlower.str();
+
+    if (ret.find("that is") == std::string::npos) {
+      ret += " that is " + color;
+    }
+    else if (ret.find(color) == std::string::npos) {
+      ret += " and " + color;
+    }
+
+    return ret;
+  }
+};
