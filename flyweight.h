207dc7a7bfa64aee03c864bd77ae723a2a35ff1e
Benjamin Mahr <Ben1980@users.noreply.github.com>
2019-08-11 13:47:36 +0200

    Create flyweight.h

---
+++ flyweight.h
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+#include <sstream>
+
+// Lightweight data representation
+// Example:
+// Sentence sentence("hello world");
+// sentence[1].capitalize = true;
+class Sentence
+{
+public:
+  Sentence(const std::string& text) {
+    std::istringstream stream(text);
+    std::string s;
+
+    while (std::getline(stream, s, ' ')) {
+      sentence.push_back( {s, false} );
+    }
+  }
+
+  struct WordToken
+  {
+    std::string word;
+    bool capitalize;
+  };
+
+  WordToken& operator[](size_t index)
+  {
+    return sentence[index];
+  }
+
+  std::string str() const
+  {
+    std::string str;
+
+    for(size_t tokeIndex = 0; tokeIndex < sentence.size(); ++tokeIndex) {
+      WordToken token = sentence[tokeIndex];
+      
+      if(token.capitalize) {
+        for(size_t charIndex = 0; charIndex < token.word.length(); ++charIndex) {
+          auto c = toupper(token.word[charIndex]);
+          token.word[charIndex] = c;
+        }
+      }
+
+      str += token.word + (tokeIndex < sentence.size()-1 ? " " : "");
+    }
+
+    return str;
+  }
+
+private:
+  std::vector<WordToken> sentence;
+};
